7b35bcb623acbda3daac5b25fd454167e9d25959
kristianmischke <kristianmischke@gmail.com>
2019-01-26 12:00:16 -0500

    Adding in Basic CutSceneController
    Cut Scenes execute actions by loading sections of an XML file in sequence to allow for dynamic control

---
+++ WhereTheHeartIs/Assets/_Scripts/CutScene/CutSceneController.cs
@@ -0,0 +1,189 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CutSceneController : MonoBehaviour
+{
+    public enum Action
+    {
+        NONE = 0,
+
+        BOUNCE,
+        WALK_CENTER,
+        WALK_OFF_LEFT,
+        WALK_OFF_RIGHT,
+
+        NUM_ACTIONS
+    }
+
+    public enum BubbleDirection
+    {
+        LEFT,
+        RIGHT
+    }
+
+    // Scene References
+    public SpriteRenderer backgroundImage;
+    public GameObject leftCharacter;
+    public SpriteRenderer leftCharacterImage;
+    public GameObject rightCharacter;
+    public SpriteRenderer rightCharacterImage;
+    public SpeechBubbleController speechBubble;
+
+
+    // Data
+    XmlDocument cutSceneDoc;
+
+    int currentSection;
+    Action leftCharacterAction;
+    Action rightCharacterAction;
+
+    bool doneCutScene;
+    float sectionTime = 0;
+    float time = 0;
+
+    Vector2 leftCharacterLocation = new Vector2(-6, -1);
+    Vector2 rightCharacterLocation = new Vector2(6, -1);
+
+
+    public bool isTesting = false;
+    public string testingFile = "";
+    public void Start()
+    {
+        if (isTesting && !string.IsNullOrEmpty(testingFile))
+        {
+            SetCutScene(testingFile);
+        }
+    }
+
+    public void SetCutScene(string file)
+    {
+        TextAsset xmlAsset = Resources.Load<TextAsset>(file);
+        cutSceneDoc = new XmlDocument();
+        cutSceneDoc.LoadXml(xmlAsset.text);
+
+        doneCutScene = false;
+        currentSection = 0;
+        time = 0;
+
+        PlayCurrentSection();
+    }
+
+    public void PlayCurrentSection()
+    {
+        if (cutSceneDoc == null)
+            return;
+
+        XmlNode root = cutSceneDoc.DocumentElement;
+
+        if (currentSection >= 0 && currentSection < root.ChildNodes.Count)
+        {
+            foreach (XmlNode node in root.ChildNodes[currentSection])
+            {
+                switch (node.Name)
+                {
+                    case "time":
+                        sectionTime = time = float.Parse(node.InnerText);
+                        break;
+                    case "backgroundImage":
+                        backgroundImage.sprite = Resources.Load<Sprite>(node.InnerText);
+                        break;
+                    case "leftCharacter":
+                        LoadCharacter(node, ref leftCharacterImage, ref leftCharacterAction);
+                        break;
+                    case "rightCharacter":
+                        LoadCharacter(node, ref rightCharacterImage, ref rightCharacterAction);
+                        break;
+                    case "speechBubble":
+                        LoadSpeechBubble(node, ref speechBubble);
+                        break;
+                }
+            }
+        }
+        else
+        {
+            doneCutScene = true;
+        }
+    }
+
+    public void LoadCharacter(XmlNode node, ref SpriteRenderer characterImage, ref Action characterAction)
+    {
+        foreach (XmlNode child in node.ChildNodes)
+        {
+            switch (child.Name)
+            {
+                case "image":
+                    characterImage.sprite = Resources.Load<Sprite>(child.InnerText);
+                    break;
+                case "action":
+                    characterAction = (Action)System.Enum.Parse(typeof(Action), child.InnerText);
+                    break;
+            }
+        }
+    }
+
+    public void LoadSpeechBubble(XmlNode node, ref SpeechBubbleController bubble)
+    {
+        foreach (XmlNode child in node.ChildNodes)
+        {
+            switch (child.Name)
+            {
+                case "text":
+                    bubble.gameObject.SetActive(!string.IsNullOrEmpty(child.InnerText));
+                    bubble.text = child.InnerText;
+                    break;
+                case "direction":
+                    bubble.flipBubble = (BubbleDirection)System.Enum.Parse(typeof(BubbleDirection), child.InnerText) == BubbleDirection.RIGHT;
+                    break;
+            }
+        }
+    }
+
+    void Update ()
+    {
+        if (doneCutScene)
+            return;
+
+        if (time <= 0)
+        {
+            // next section
+            currentSection++;
+            PlayCurrentSection();
+        }
+        else
+        {
+            time -= Time.deltaTime;
+
+            float percentDone = sectionTime <= 0 ? 0 : (sectionTime - time) / sectionTime;
+
+            // execute actions here
+            ExecuteCharacterAction(leftCharacter, leftCharacterAction, leftCharacterLocation, percentDone);
+            ExecuteCharacterAction(rightCharacter, rightCharacterAction, rightCharacterLocation, percentDone);
+
+        }
+	}
+
+
+    private void ExecuteCharacterAction(GameObject characterObj, Action characterAction, Vector2 characterNormalPosition, float percentDone)
+    {
+        switch (characterAction)
+        {
+            case Action.NONE:
+                return;
+            case Action.BOUNCE:
+                characterObj.transform.position = characterNormalPosition + Vector2.up * Mathf.Sin(percentDone * Mathf.PI);
+                break;
+            case Action.WALK_CENTER:
+                characterObj.transform.position = Vector2.Lerp(characterNormalPosition, new Vector2(0, characterNormalPosition.y), percentDone);
+                break;
+            case Action.WALK_OFF_LEFT:
+                characterObj.transform.position = Vector2.Lerp(characterNormalPosition, new Vector2(-12, characterNormalPosition.y), percentDone);
+                break;
+            case Action.WALK_OFF_RIGHT:
+                characterObj.transform.position = Vector2.Lerp(characterNormalPosition, new Vector2(12, characterNormalPosition.y), percentDone);
+                break;
+        }
+    }
+}
---
+++ WhereTheHeartIs/Assets/_Scripts/CutScene/SpeechBubbleController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class SpeechBubbleController : MonoBehaviour
+{
+    
+    public string text;
+    public TMP_Text textObj;
+    public Vector2 margin;
+    public bool flipBubble;
+
+    Image bubbleImage;
+	
+	void Start ()
+    {
+        bubbleImage = GetComponent<Image>();
+	}
+	
+	
+	void Update ()
+    {
+        if (textObj != null)
+        {
+            textObj.text = text;
+        }
+    }
+
+    void LateUpdate()
+    {
+        if (textObj != null)
+        {
+            textObj.text = text;
+            GetComponent<RectTransform>().sizeDelta = textObj.GetComponent<RectTransform>().sizeDelta + (margin * 2);
+
+            transform.localScale = new Vector3((flipBubble ? -1 : 1), 1, 1);
+            textObj.transform.localScale = new Vector3((flipBubble ? -1 : 1), 1, 1);
+        }
+    }
+}
