95dc5a7bbb94306d88b37d1fd0fd19fe2bfc1e60
Craig <craigwstone@gmail.com>
2019-01-25 23:37:28 -0500

    Added mechanics and staging for the platformer minigame

---
+++ WhereTheHeartIs/Assets/_Scripts/MiniGames/Platformer/CubeController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+
+    public BoxCollider2D cubeCollider;
+    public Rigidbody2D cubeRigidBody;
+
+    private bool IsGrounded = true;
+	// Use this for initialization
+	void Start ()
+    {
+        cubeCollider = GetComponent<BoxCollider2D>();
+        cubeRigidBody = GetComponent<Rigidbody2D>();
+
+	}
+	
+	
+	void FixedUpdate ()
+    {
+        Vector2 positionx = new Vector2(Input.GetAxis("Horizontal") * 10f, 0f);
+        Vector2 positiony = new Vector2(0f, 1f);
+        cubeRigidBody.AddForce(positionx, ForceMode2D.Force);
+
+
+        //ContactFilter2D GroundFilter = new ContactFilter2D();
+        //GroundFilter.
+        RaycastHit2D GroundRay = Physics2D.Raycast(transform.position, Vector2.down);
+        //bool Ray = Physics.Raycast(transform.position, Vector3.down);
+        //Debug.Log(Ray);
+        //Debug.DrawLine(transform.position, new Vector3(GroundRay.point.x, GroundRay.point.y, transform.position.z));
+        float distance;
+
+        if (GroundRay.collider != null && GroundRay.collider != cubeCollider)
+        {
+            distance = Mathf.Abs(GroundRay.point.y - transform.position.y);
+            //Debug.Log(distance);
+            IsGrounded = distance <= 1f;
+        }
+
+        //IsGrounded = Physics2D.Raycast(transform.position, Vector2.down, 1.5f).distance <= 1.5f;
+        //Debug.Log(Physics2D.Raycast(transform.position, Vector2.down, 1.5f).distance);
+        if (IsGrounded && Input.GetButton("Jump"))
+        {
+            cubeRigidBody.AddForce(positiony, ForceMode2D.Impulse);
+        }
+
+
+
+	}
+}
---
+++ WhereTheHeartIs/Assets/_Scripts/MiniGames/Platformer/FallOffWorld.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallOffWorld : MonoBehaviour {
+
+    public BoxCollider2D Pit;
+
+	// Use this for initialization
+	void Start () {
+        Pit = GetComponent<BoxCollider2D>();
+        //Player = GetComponent<BoxCollider2D>();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //Debug.Log("Yes");
+        other.transform.position = new Vector2(-6f,-0.5f);
+        other.GetComponent<Rigidbody2D>().velocity = new Vector2(0.0f, 0.0f);
+    }
+}
