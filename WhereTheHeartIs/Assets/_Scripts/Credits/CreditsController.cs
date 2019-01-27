0bfcc0db5e3d3368ace73ef45400f2ebc6ccd010
Craig <craigwstone@gmail.com>
2019-01-27 15:08:08 -0500

    Credits Scene

---
+++ WhereTheHeartIs/Assets/_Scripts/Credits/CreditsController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class CreditsController : MonoBehaviour {
+
+    public Button MenuButton;
+
+	// Use this for initialization
+	void Start () {
+        MenuButton.onClick.AddListener(() => { SceneManager.LoadScene("MainMenuScene"); });
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
